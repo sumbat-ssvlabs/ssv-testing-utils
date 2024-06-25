91f1e97a08f3511dfc325bf30dfdea51b4056b28
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-06-25 15:34:56 +0300

    feat: added rsa generator

---
+++ app/lib/generateKey.ts
@@ -0,0 +1,16 @@
+import NodeRSA from "node-rsa";
+import Base64 from "base-64";
+
+const key = new NodeRSA({ b: 2048 });
+
+export const generateKey = () => {
+  const generated = key.generateKeyPair();
+  const publicKey = generated.exportKey("public");
+  const withRSA = `${publicKey.replaceAll("PUBLIC", "RSA PUBLIC")}
+`;
+  const encoded = Base64.encode(withRSA).replace(/\n/g, "");
+  return {
+    encoded,
+    withRSA,
+  };
+};
