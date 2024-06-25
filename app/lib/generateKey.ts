999e9da84ca20e362ae38cb3adaebd97eef32b1b
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-06-25 17:04:04 +0300

    fix2

--- app/lib/generateKey.ts
+++ app/lib/generateKey.ts
@@ -2,9 +2,9 @@
 
 import NodeRSA from "node-rsa";
 import Base64 from "base-64";
+const key = new NodeRSA({ b: 2048 });
 
 export const generateKey = () => {
-  const key = new NodeRSA({ b: 2048 });
   const generated = key.generateKeyPair();
   const publicKey = generated.exportKey("public");
   const withRSA = `${publicKey.replaceAll("PUBLIC", "RSA PUBLIC")}
