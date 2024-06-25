50173c6916a5b983de101d146b934b77a8856a55
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-06-25 15:44:37 +0300

    fix

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <main className="min-h-screen p-24">
+      <h1 className="text-4xl font-bold text-center mb-8">
+        RSA Public Key Generator
+      </h1>
+      <span>Loading...</span>
+    </main>
+  );
+}
