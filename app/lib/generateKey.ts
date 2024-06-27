6213bcea1856144eab1174159230d519289211f2
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-06-27 14:59:20 +0300

    fix: clientside creation

--- app/lib/generateKey.ts
+++ app/lib/generateKey.ts
@@ -1,17 +1,17 @@
-"use server";
+"use client";
 
 import NodeRSA from "node-rsa";
 import Base64 from "base-64";
+
 const key = new NodeRSA({ b: 2048 });
 
 export const generateKey = async () => {
-  const generated = key.generateKeyPair();
-  const publicKey = generated.exportKey("public");
-  const withRSA = `${publicKey.replaceAll("PUBLIC", "RSA PUBLIC")}
-`;
-  const encoded = Base64.encode(withRSA).replace(/\n/g, "");
-  return {
-    encoded,
-    withRSA,
-  };
+  return new Promise((resolve) => {
+    const generated = key.generateKeyPair();
+    const publicKey = generated.exportKey("public");
+    const withRSA = `${publicKey.replaceAll("PUBLIC", "RSA PUBLIC")}
+  `;
+    const encoded = Base64.encode(withRSA).replace(/\n/g, "");
+    resolve({ encoded, publicKey, withRSA });
+  });
 };
