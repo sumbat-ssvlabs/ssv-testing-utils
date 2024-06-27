6a5b1a997d4825ba418d35ea596db5fa04f2eb8b
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-06-27 15:02:16 +0300

    title + img

--- app/components/MainPage.tsx
+++ app/components/MainPage.tsx
@@ -2,6 +2,7 @@
 
 import { useMutation } from "@tanstack/react-query";
 import { generateKey } from "../lib/generateKey";
+import Image from "next/image";
 
 export const MainPage = () => {
   const mutation = useMutation({
@@ -16,8 +17,15 @@ export const MainPage = () => {
 
   return (
     <main className="min-h-screen p-24 max-w-screen-lg mx-auto">
+      <Image
+        src="https://app.stage.ssv.network/images/logo/dark.svg"
+        width={120}
+        height={120}
+        alt="SSV Logo"
+        className="mx-auto mb-8"
+      />
       <h1 className="text-4xl font-bold text-center mb-8">
-        RSA Public Key Generator
+        Operator Public Key Generator
       </h1>
       {mutation.isPending ? (
         <p className="text-center mb-8">Generating...</p>
@@ -30,7 +38,7 @@ export const MainPage = () => {
         </p>
       )}
       <textarea
-        rows={10}
+        rows={8}
         value={mutation.isSuccess ? mutation.data?.keys.encoded : ""}
         className="flex  w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
       />
