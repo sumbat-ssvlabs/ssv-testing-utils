1faf523f837851fba79cf1ae64a9cc2a56f94cec
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-07-01 09:34:47 +0300

    fix: generateKey

--- app/lib/generateKey.ts
+++ app/lib/generateKey.ts
@@ -5,17 +5,14 @@ import Base64 from "base-64";
 
 const key = new NodeRSA({ b: 2048 });
 
-export const generateKey = async (): Promise<{
-  encoded: string;
-  publicKey: string;
-  withRSA: string;
-}> => {
-  return new Promise((resolve) => {
-    const generated = key.generateKeyPair();
-    const publicKey = generated.exportKey("public");
-    const withRSA = `${publicKey.replaceAll("PUBLIC", "RSA PUBLIC")}
-  `;
-    const encoded = Base64.encode(withRSA).replace(/\n/g, "");
-    resolve({ encoded, publicKey, withRSA });
-  });
+export const generateKey = async () => {
+  const generated = key.generateKeyPair();
+  const publicKey = generated.exportKey("public");
+  const withRSA = `${publicKey.replaceAll("PUBLIC", "RSA PUBLIC")}
+`;
+  const encoded = Base64.encode(withRSA).replace(/\n/g, "");
+  return {
+    encoded,
+    withRSA,
+  };
 };
